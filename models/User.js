573dc49c83965516cfb333b4a70ab9593625f582
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-02-10 17:51:06 -0800

    'User model added bcrypt for PW'

--- models/User.js
+++ models/User.js
@@ -55,6 +55,34 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// For hashing the password - NOTE: these hooks are not executed on update() and findOneAndUpdate()
+UserSchema.pre("save", function (next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
+
+  // generate a salt
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
