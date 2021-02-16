df1928043dc3bbd01e3d5f78b6fe07b1879f9343
marandaz <mrzwies@gmail.com>
2021-02-15 20:17:49 -0600

    adding deets for the recipe page so I have something to work with

--- client/src/pages/recipe.js
+++ client/src/pages/recipe.js
@@ -8,7 +8,10 @@ export const Recipe = () => {
     return (
       <Container fluid>
         <h1>
-          events
+          Welcome to the Recipe page! This page will display recipes that match a users search criteria.
+          
+          A user will get here from the EVENTS/MENU page.
+          
         </h1>
         
       </Container>
