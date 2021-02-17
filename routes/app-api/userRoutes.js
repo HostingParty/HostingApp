42dada96089a950d8f9d5b81bd6095efe34b92d9
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-02-17 02:27:36 -0800

    'Passport and auth middleware'

--- routes/app-api/userRoutes.js
+++ routes/app-api/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const isLoggedIn = require("../../middleware/isLoggedIn");
 const {
   getUsers,
   getSingleUser,
@@ -12,8 +13,8 @@ const {
 //Below routers are set to path /api/v1/users
 router.route("/").get(getUsers).post(addUser);
 
-router.route("/pw/:id").put(updateUserPw);
+router.route("/pw/:id").put(isLoggedIn, updateUserPw);
 
-router.route("/:id").get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/:id").get(getSingleUser).put(isLoggedIn, updateUser).delete(isLoggedIn, deleteUser);
 
 module.exports = router;
