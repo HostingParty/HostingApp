45513a66b82504d1e9f98d1ac5c4d922d673752e
Ben Robinson <robinson.dri@gmail.com>
2021-02-17 20:11:14 -0800

    bringing over Brandon's changes

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -19,40 +19,14 @@ import MailIcon from "@material-ui/icons/Mail";
 class Login extends React.Component {
 constructor(props) {
 super(props);
-this.state = { username: "", password:"", authflag:1 };
-this.handleChange = this.handleChange.bind(this);
-this.handleSubmit = this.handleSubmit.bind(this);
-}
-handleChange(event) {
-this.setState({ username: event.state.username, password: event.state.password });
-}
-handleSubmit(event) {
-event.preventDefault();
-if (this.state.username == 'admin@littech.in' && this.state.password == 'secret') {
-this.props.history.push("/home");
-} else {
-alert('Incorrect Credentials!');
-}
+
 }
 render() {
 return (
 <div>
 
   <NavBar />
-{/* <AppBar position="static" alignitems="center" color="primary">
-<Toolbar>
-<Grid container justify="center" wrap="wrap">
-<Grid item>
-<Typography variant="h6">Hosting Party</Typography>
-</Grid>
-</Grid>
-<IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-</Toolbar>
-</AppBar> */}
+
 <Grid container spacing={0} justify="center" direction="row">
 <Grid item>
 <Grid container direction="column" justify="center" spacing={2} className="login-form"
---
+++ client/src/pages/Profile/index.js
@@ -0,0 +1,128 @@
+import React from "react";
+import "./style.css";
+import NavBar from "../../components/Nav/index";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "../../components/Avatar/index";
+import {
+Button,
+TextField,
+Grid,
+Paper,
+AppBar,
+Typography,
+Toolbar,
+Link,
+IconButton,
+Badge,
+FormLabel,
+FormControl,
+FormGroup,
+FormControlLabel,
+FormHelperText,
+Checkbox,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
+  }));
+  
+  
+function Profile() {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+      fish: true,
+      peanuts: false,
+      dary: false,
+      chocolate: false,
+      none: false,
+      italian: false,
+      steak: false,
+      vegies: false,
+      fingerFoods: false,
+      pizza: false
+    });
+  
+    const handleChange = (event) => {
+      setState({ ...state, [event.target.name]: event.target.checked });
+    };
+  
+    const { fish, peanuts, dary, chocolate, none, italian, steak, vegies, fingerFoods, pizza } = state;
+    const error = [fish, peanuts, dary, chocolate, none, italian, steak, vegies, fingerFoods, pizza].filter((v) => v).length !== 2;
+
+    // const { italian, steak, vegies, fingerFoods, pizza } = state;
+    // const error = [italian, steak, vegies, fingerFoods, pizza].filter((v) => v).length !== 2;
+
+    return(
+        <div className={classes.root}>
+        <NavBar />
+        <h2>Welcome <small>"User Name"</small></h2>
+        <Avatar />
+     
+                
+        <Grid container spacing={0} justify="center" direction="row">
+        
+        <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Allergies</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={fish} onChange={handleChange} name="fish" />}
+            label="Fish"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={peanuts} onChange={handleChange} name="peanuts" />}
+            label="Peanuts"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={dary} onChange={handleChange} name="dary" />}
+            label="Dary"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={chocolate} onChange={handleChange} name="chocolate" />}
+            label="Chocolate"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={none} onChange={handleChange} name="none" />}
+            label="None"
+          />
+        </FormGroup>        
+        <FormHelperText></FormHelperText>
+        
+      </FormControl>
+     
+ <FormControl required error={error} component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Prefrences</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={italian} onChange={handleChange} name="italian" />}
+            label="Italian"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={steak} onChange={handleChange} name="steak" />}
+            label="Steak"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={vegies} onChange={handleChange} name="vegies" />}
+            label="Vegies"
+          />
+             <FormControlLabel
+            control={<Checkbox checked={fingerFoods} onChange={handleChange} name="fingerFoods" />}
+            label="Finger Foods"
+          />
+             <FormControlLabel
+            control={<Checkbox checked={pizza} onChange={handleChange} name="pizza" />}
+            label="Pizza"
+          />
+        </FormGroup>
+        <FormHelperText></FormHelperText>
+      </FormControl> 
+      </Grid>
+        </div>
+    )
+}
+
+export default Profile;
