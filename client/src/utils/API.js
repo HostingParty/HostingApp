4c17131d97f19599f184bb1d815bee6b3f616342
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-02-18 14:59:30 -0800

    'Update Client API Calls'

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,40 @@ exports.login = function (user) {
   return axios.post("/auth/login", user);
 };
 
+// User
 exports.registerUser = function (user) {
   return axios.post("/api/v1/users", user);
+};
+
+exports.getUserInfo = function (id) {
+  return axios.get("/api/v1/user/" + id);
+};
+
+exports.updateUserInfo = function (id, body) {
+  return axios.put("/api/v1/user/" + id, body);
+};
+
+exports.updateUserPw = function (id, body) {
+  return axios.put("/api/v1/user/pw/" + id, body);
+};
+
+exports.deleteUser = function (id) {
+  return axios.delete("/api/v1/user/" + id);
+};
+
+// Events
+exports.addEvent = function (body) {
+  return axios.post("/api/v1/events", body);
+};
+
+exports.getEventInfo = function (eventId) {
+  return axios.get("/api/v1/events/" + eventId);
+};
+
+exports.updateEventInfo = function (eventId, body) {
+  return axios.put("/api/v1/events/" + eventId, body);
+};
+
+exports.deleteEvent = function (eventId) {
+  return axios.delete("/api/v1/events/" + eventId);
 };
