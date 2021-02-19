8807f026b591698269086c55e6a55b77db344908
marandaz <mrzwies@gmail.com>
2021-02-19 09:50:06 -0600

    pulling

--- client/src/utils/globalState.js
+++ client/src/utils/globalState.js
@@ -1,58 +1,28 @@
 import React, { createContext, useReducer, useContext } from "react";
-import {
-  ADD_INVITE,
-  REMOVE_INVITE
-} from "./actions";
 
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case ADD_INVITE:
-      return {
-        ...state,
-        pendingInvites: [action.post, ...state.pendingInvites],
-        loading: false
-      };
+    return {
+      ...state
+    };
 
-    case REMOVE_INVITE:
-      return {
-        ...state,
-        pendingInvites: [...state.pendingInvites],
-        loading: false
-      };
-
-    default:
-      return state;
-    }
+    
 };
 
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useReducer(reducer, {
-    event: {
-      details: {
-        _id: 0,
-        name: "State Event Thing",
-        date: "2/16/2021",
-        time: "noon",
-        address: "101 NE 45th 981226",
-        notes: "B there or b square."
-      },
-      guestList: {
-        pendingInvites: ["Dan", "Ben"],
-        attendingInvites: ["Brandon"],
-        maybeInvites: ["Maranda"],
-        declinedInvites: ["BenTA"],
-      },
-      menu: {
-        apps: ["Chips", "Dip", "Salsa"],
-        sides: ["Green Salad", "Bread sticks"],
-        mains: ["Turducken"]
-      }
-    },
-      loading: false
+    // posts: [],
+    // currentPost: {
+    //   _id: 0,
+    //   title: "",
+    //   body: "",
+    //   author: ""
+    // },
+    // favorites: [],
+    // loading: false
   });
 
   return <Provider value={[state, dispatch]} {...props} />;
