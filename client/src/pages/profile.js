4ca520d73937f3c2967893d93abe74c46fd2adcc
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-02-18 18:41:06 -0800

    'update Profile with useEffect to grab user information'

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,13 +1,25 @@
-import React from "react";
-import Container from '@material-ui/core/Container';
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../utils/Store";
+import Container from "@material-ui/core/Container";
+import API from "../utils/API";
 
 export const Profile = () => {
+  const [state, dispatch] = useContext(Context);
+  const [profile, setProfile] = useState({});
+
+  const userId = state.user.id;
+
+  useEffect(() => {
+    API.getUserInfo(userId).then((profileInfo) => {
+      let data = profileInfo.data.data[0];
+      console.log(data);
+      setProfile(data);
+    });
+  }, [state]);
+
   return (
     <Container fluid>
-    <h1>
-      Profile
-    </h1>
-    
-  </Container>
+      <h1>Hello, {profile.name.first}</h1>
+    </Container>
   );
 };
