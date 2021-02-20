8530faf36321b586ac7e08d36d64fb5c9db30a21
marandaz <mrzwies@gmail.com>
2021-02-20 14:40:55 -0600

    removed extra code

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import { useStoreContext } from "../../utils/GlobalState";
-
-// function Nav() {
-//   const [store] = useStoreContext();
-
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <a className="navbar-brand" href="/">
-//         Your personal CMS
-//       </a>
-//       {store.loading ? <a className="navbar-brand ml-auto">Loading...</a> : <></>}
-//     </nav>
-//   );
-// }
-
-// export default Nav;
-
 import React from 'react';
 import { Link } from "react-router-dom";
 import clsx from 'clsx';
