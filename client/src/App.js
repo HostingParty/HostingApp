41bc75b96724a1c71e6ac9bae55e6e8269f95fc0
Dan Cornutt <dancornutt@Dans-MacBook-Pro.local>
2021-02-22 18:38:17 -0800

    removed route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,6 @@ function App() {
           {/* <Route exact path="/" component={CreateUser} /> */}
           <Route exact path="/login" component={Login} />
           <Route exact path="/createEvent" component={CreateEvent} />
-          {/* <Route exact path="/events" component={Events} /> */}
           <Route exact path="/event" component={Event} /> 
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/recipe" component={Recipe} />
