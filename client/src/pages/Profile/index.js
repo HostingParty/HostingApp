4f425989e4e266f70294cf1051d0cb27369c54c5
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-02-26 21:27:15 -0800

    'axios route for profilepic upload updated'

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -69,7 +69,7 @@ function Profile() {
       "Content-Type": "multipart/form-data",
     };
 
-    await axios("/api/aws", formData, { headers: headers });
+    await axios("/api/v1/upload-photo", formData, { headers: headers });
   };
 
   const cancelUpload = (e) => {
