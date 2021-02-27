722ebd78f443c4c4e524058cf78b154e8934484d
Dan Cornutt <dancornutt@Dans-MacBook-Pro.local>
2021-02-26 19:34:57 -0800

    Changed design pattern for AWS controller/routes. No routes file needed

--- controllers/awsController.js
+++ controllers/awsController.js
@@ -5,81 +5,64 @@ const router = require("express").Router();
 const AWS = require('aws-sdk');
 const multer = require("multer");
 
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
+module.exports = function(app) {
+  var storage = multer.memoryStorage();
+  var upload = multer({ storage: storage });
 
-//POST route to upload a document file. 
-//Req has: {file.originalname, userId}
-exports.uploadPhoto(upload.single("myFile")) = async (req, res, next) => {
-  if (req.file.userId) {
+  // app.post("/upload-photo", upload.single("myPicture"), function(req, res) {
+  app.post("/upload-photo", function(req, res) {
+    if (req.file.userId) {
 
-    const file = req.file;
-    const s3FileURL = process.env.AWS_S3_Uploaded_File_URL_LINK;
+      const file = req.file;
+      const s3FileURL = process.env.AWS_S3_Uploaded_File_URL_LINK;
 
-    let s3bucket = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_S3_REGION
-    });
+      let s3bucket = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_S3_REGION
+      });
 
-    //Stored file metadata
-    var params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: file.originalname,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-      ACL: "public-read"
-    };
+      //Stored file metadata
+      var params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: "public-read"
+      };
 
-    // //Call to S3 storage, uploading picture
-    s3bucket.upload(params, function(err, data) {
-      if (err) {
-        res.status(500).json({ error: true, Message: err });
-      } else {
-        res.send({ data });
-        var newFileUploaded = {
-          description: req.body.description,
-          fileLink: s3FileURL + file.userId, //we know url. /userID should just be the default place
-          s3_key: params.Key
-        };
-      }
-    });
-  }   
-}
-
-exports.textUser() = async (req, res, next) => {
-  notifyAttendees(`+12064120323`, "hello world");
-  // function fetchSubscribers(event, msg) {
-  //   // NEEED: DB call for list of attendees for a particular event
-  //   db.User.findAll({
-  //       //where textNotification: true & on event: req.event
-  //   }).then(function(data) {
-  //     console.log("Users to be notified: ");
-  //     data.forEach(element => {
-  //       notifyAttendees(`+1${element.phoneNumber}`, req.msg);
-  //     });
-  //   })
-  // }
-}
-
-function notifyAttendees(num, msg) {
-  //Foreach phone number, send text;
-  let params = {
-    Message: msg,
-    PhoneNumber: num,
-    MessageAttributes: {
-        'AWS.SNS.SMS.SenderID': {
-            'DataType': 'String',
-            'StringValue': "Hosting Party"
+      // //Call to S3 storage, uploading picture
+      s3bucket.upload(params, function(err, data) {
+        if (err) {
+          res.status(500).json({ error: true, Message: err });
+        } else {
+          res.send({ data });
+          var newFileUploaded = {
+            description: req.body.description,
+            fileLink: s3FileURL + file.originalname,
+            s3_key: params.Key
+          };
         }
+      });
     }
-  };
-  const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
-  publishTextPromise.catch(
-    function (err) {
-        console.log(`An SNS error has occured: ${err}`);
-    }
-  )
-};
+  });
 
-module.exports = router;
+  app.post("/text/:number", function(req, res) {
+      let params = {
+        Message: req.body,
+        PhoneNumber: req.params.number,
+        MessageAttributes: {
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': "PNWWhaler"
+            }
+        }
+      };
+      const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+      publishTextPromise.catch(
+        function (err) {
+            console.log(`An SNS error has occured: ${err}`);
+        }
+      )
+    });
+}
--- routes/aws-api/awsRoutes.js
+++ routes/aws-api/awsRoutes.js
@@ -1,12 +1,75 @@
-const express = require("express");
-const router = express.Router();
-const { uploadPhoto } = require("../../controllers/awsController");
+// const express = require("express");
+// const router = express.Router();
+// const AWS = require('aws-sdk');
+// const multer = require("multer");
 
-//Below routers are set to path /api/v1/aws
-router.route("/upload-photo")
-    .post(uploadPhoto)
+// //POST route to upload a picture file
+// router.post("/upload-photo", upload.single("myPicture"), function(req, res) {
+//     var storage = multer.memoryStorage();
+//     var upload = multer({ storage: storage });
+//   if (req.file.originalname) {
 
-router.route("/text/:userId")
-    .post(textUser)
+//     const file = req.file;
+//     const s3FileURL = process.env.AWS_S3_Uploaded_File_URL_LINK;
 
-module.exports = router;
+//     let s3bucket = new AWS.S3({
+//       accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+//       secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+//       region: process.env.AWS_S3_REGION
+//     });
+
+//     //Stored file metadata
+//     var params = {
+//       Bucket: process.env.AWS_S3_BUCKET_NAME,
+//       Key: file.originalname,
+//       Body: file.buffer,
+//       ContentType: file.mimetype,
+//       ACL: "public-read"
+//     };
+
+//     //Call to S3 storage, uploading picture
+//     s3bucket.upload(params, function(err, data) {
+//       if (err) {
+//         res.status(500).json({ error: true, Message: err });
+//       } else {
+//         res.send({ data });
+//         var newFileUploaded = {
+//           description: req.body.description,
+//           fileLink: s3FileURL + file.originalname,
+//           s3_key: params.Key
+//         };
+
+//         //Put update NEEDED to user with image url of fileLink    
+
+//       }
+//     })      
+//   }
+// });
+
+// router.post("/text/:number", function(req, res) {
+//     let num = req.params.number;
+//     let msg = req.body;
+//     notifySubscribers(num, msg);
+// });
+
+// function notifySubscribers(num, msg) {
+// //Foreach subscriber, send text;
+// let params = {
+//     Message: msg,
+//     PhoneNumber: num,
+//     MessageAttributes: {
+//         'AWS.SNS.SMS.SenderID': {
+//             'DataType': 'String',
+//             'StringValue': "Hosting Party"
+//         }
+//     }
+// };
+// const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+// publishTextPromise.catch(
+//     function (err) {
+//         console.log(`An SNS error has occured: ${err}`);
+//     }
+// )
+// };
+
+// module.exports = router;
